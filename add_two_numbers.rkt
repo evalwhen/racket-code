faf4a5408b2861252e2b2bb5cce8fb9058c5a218
melp <huzi.master@outlook.com>
2020-03-04 21:23:14 +0800

    add: add two numbers.

--- add_two_numbers.rkt
+++ add_two_numbers.rkt
@@ -6,9 +6,31 @@
     [(null? l1) (if (= c 0) '() '(1))]
     [else
      (let ([sum (+ (car l1) (car l2) c)])
-       (cons (if (>= sum 10)
-                 (- sum 10)
-                 sum)
-             (add-two-sum (cdr l1)
-                          (cdr l2)
-                          1)))]))
+       (cond
+         [(>= sum 10) (cons (- sum 10)
+                            (add-two-sum (cdr l1)
+                                         (cdr l2)
+                                         1))]
+         [else (cons sum
+                     (add-two-sum (cdr l1)
+                                  (cdr l2)
+                                  0))]))]))
+
+
+(define (add-two-sum2 l1 l2 c r)
+  (cond
+    [(null? l1) (reverse (if (= c 0) r (cons 1 r)))]
+    [else
+     (let ([sum (+ (car l1) (car l2) c)])
+       (if (>= sum 10)
+           (add-two-sum2 (cdr l1)
+                        (cdr l2)
+                        1
+                        (cons (- sum 10) r))
+           (add-two-sum2 (cdr l1)
+                        (cdr l2)
+                        0
+                        (cons sum r))))]))
+
+(add-two-sum '(1 3 4) '(2 4 6) 0)
+(add-two-sum2 '(1 3 4) '(2 4 6) 0 '())
