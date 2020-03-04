bd010b930ea540e3b8abccb37877e498d6e5aeed
melp <huzi.master@outlook.com>
2020-03-04 18:18:22 +0800

    format.

--- 3sum.rkt
+++ 3sum.rkt
@@ -1,11 +1,16 @@
 #lang racket
 
+(provide 3sum)
 ;; 找出 l 中值为 x 的下标
 (define (eq-v x l i)
   (cond
     [(null? l) '()]
-    [(= x (car l)) (cons i (eq-v x (cdr l) (+ i 1)))]
-    [else (eq-v x (cdr l) (+ i 1))]))
+    [(= x (car l)) (cons i (eq-v x
+                                 (cdr l)
+                                 (+ i 1)))]
+    [else (eq-v x
+                (cdr l)
+                (+ i 1))]))
 
 ;; 找出 l 中和为 v 的二元组
 (define (2sum-eq-v v l j)
@@ -13,8 +18,12 @@
     [(< (length l) 2) '()]
     [else
      (append
-      (make-tuble j (eq-v (- v (car l)) (cdr l) (+ j 1)))
-      (2sum-eq-v v (cdr l) (+ j 1)))]))
+      (make-tuble j (eq-v (- v (car l))
+                          (cdr l)
+                          (+ j 1)))
+      (2sum-eq-v v
+                 (cdr l)
+                 (+ j 1)))]))
 
 ;; (f3 2 '(1 2 3)) => '((2 1) (2 2) (2 3))
 (define (make-tuble i l)
