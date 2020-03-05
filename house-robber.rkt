682c4f6caf8b91143add9fc95184fac51571fba2
melp <huzi.master@outlook.com>
2020-03-05 17:56:27 +0800

    add: hourse robber.

--- house-robber.rkt
+++ house-robber.rkt
@@ -1,22 +1,23 @@
 #lang eopl
 
 ;; https://leetcode.com/problems/house-robber/description/
-(define (f l i)
+
+(define (robe l)
+  (helper l (length l)))
+
+(define (helper l i)
   (cond
     [(<= i 0) 0]
     [(= i 1) (car l)]
     [(= i 2) (max (car l) (cadr l))]
-    [else (max (f (cdr l) (- i 1))
-               (+ (car l) (f (cddr l) (- i 2))))]))
+    [else (max (helper (cdr l) (- i 1))                  ;; 不偷第一家的情况
+               (+ (car l) (helper (cddr l) (- i 2))))])) ;; 偷第一家的情况, (偷第一家就不能偷第二家)
 
 (define (f-iter l c)
   (let loop ([a 0] [b 0] [l l] [c c])
     (cond
       [(= c 0) b]
       [else (loop b (max b (+ a (car l))) (cdr l) (- c 1))])))
-
-(define (rob l)
-  (f l (length l)))
 
 (define (f-c l i cont)
   (cond
@@ -26,3 +27,5 @@
     [else (f-c (cdr l) (- i 1) (lambda (v1)
                                  (f-c (cddr l) (- i 2) (lambda (v2)
                                                          (cont (max v1 (+ (car l) v2)))))))]))
+
+(robe '(2 7 9 3 1))
